14f1780b327a6ab4766a7da2fe4d6e53176a2108
Joel Meza <124853523+Zicrown@users.noreply.github.com>
2023-10-10 02:22:16 -0400

    agregrando librerias y creando contenido

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'desafio2';
+  datosCard=[
+    {
+      id:1,
+      titulo: 'Articulo 1',
+      precio: Math.round(Math.random()*100),
+      tipoEnvio: 'Express',
+      imagen: 'https://m.media-amazon.com/images/I/81lL5QxvDPL._AC_UL320_.jpg'
+    },
+    {
+      id:2,
+      titulo: 'Articulo 2',
+      precio: Math.round(Math.random()*100),
+      tipoEnvio: 'Naval',
+      imagen: 'https://m.media-amazon.com/images/I/71M5HhRgdeL._AC_UL320_.jpg'
+    },
+    {
+      id:3,
+      titulo: 'Articulo 3',
+      precio: Math.round(Math.random()*100),
+      tipoEnvio: 'Tierra',
+      imagen: 'https://m.media-amazon.com/images/I/61N31efNYxL._AC_UL320_.jpg'
+    },
+    {
+      id:4,
+      titulo: 'Articulo 4',
+      precio: Math.round(Math.random()*100),
+      tipoEnvio: 'MRW',
+      imagen: 'https://m.media-amazon.com/images/I/51-eyeH8+nL._AC_UL320_.jpg'
+    },
+    {
+      id:5,
+      titulo: 'Articulo 5',
+      precio: Math.round(Math.random()*100),
+      tipoEnvio: 'Vuelo',
+      imagen: 'https://m.media-amazon.com/images/I/616NIy5Td7S._AC_UL320_.jpg'
+    },
+  ]
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MdbCarouselModule } from 'mdb-angular-ui-kit/carousel';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MdbCarouselModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
